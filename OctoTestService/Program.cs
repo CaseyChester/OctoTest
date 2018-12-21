0329b4fdab915f17b43f05f2c7a595595b6ad9d7
Casey Chester <casey.chester@hotmail.com>
2018-12-21 13:18:00 -0600

    Initial commit

---
+++ OctoTestService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoTestService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText($"{DateTime.Now:yyyyMMdd-hhmmss}.txt", $"It is {DateTime.Now.ToShortTimeString()} and all is well!");
+        }
+    }
+}
